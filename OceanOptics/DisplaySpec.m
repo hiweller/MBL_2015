fb99b0a32c2125a43b1ba4403823b1dfc9e564f8
arummel25 <arummel25@gmail.com>
2015-06-27 16:33:03 -0400

    new displayspec

--- OceanOptics/DisplaySpec.m
+++ OceanOptics/DisplaySpec.m
@@ -1,28 +1,39 @@
+function DisplaySpec(Date)
 
-clear;
 wl = 400:700;
-load spec/Spec_ch0_Spec.dat;
-load spec/Spec_ch1_Spec.dat;
-load spec/Spec_ch3_Spec.dat;
-load spec/Spec_ch4_Spec.dat;
-load spec/Spec_ch6_Spec.dat;
+
+% for i = 1:5;
+%     channel(num2str(i)) = [Date,'/Spec_ch(num2str(i))_Spec.dat'];
+%     load channel(num2str(i));
+% end
+
+channel0 = [Date,'/Spec_ch0_Spec.dat'];
+channel1 = [Date,'/Spec_ch1_Spec.dat'];
+channel3 = [Date,'/Spec_ch3_Spec.dat'];
+channel4 = [Date,'/Spec_ch4_Spec.dat'];
+channel6 = [Date,'/Spec_ch6_Spec.dat'];
+
+load channel0;
+load channel1;
+load channel3;
+load channel4;
+load channel6;
+
 Spec_ch0_Spec = Spec_ch0_Spec(:,101:401); % only use from 400 to 700 nm
 Spec_ch1_Spec = Spec_ch1_Spec(:,101:401);
 Spec_ch3_Spec = Spec_ch3_Spec(:,101:401);
 Spec_ch4_Spec = Spec_ch4_Spec(:,101:401);
 Spec_ch6_Spec = Spec_ch6_Spec(:,101:401);
 
+for i = 1:size(Spec_ch0_Spec,1)
 figure
-plot(wl,Spec_ch0_Spec(1,:),'b'); hold on;
-plot(wl,Spec_ch1_Spec(1,:),'c');
-plot(wl,Spec_ch3_Spec(1,:),'g');
-plot(wl,Spec_ch4_Spec(1,:),'m');
-plot(wl,Spec_ch6_Spec(1,:),'r'); hold off;
+plot(wl,Spec_ch0_Spec(i ,:),'b'); hold on;
+plot(wl,Spec_ch1_Spec(i,:),'c');
+plot(wl,Spec_ch3_Spec(i,:),'g');
+plot(wl,Spec_ch4_Spec(i,:),'m');
+plot(wl,Spec_ch6_Spec(i,:),'r'); hold off;
+title(['Measurement number: ' num2str(i_measurement)]);
+legend({'ch 0 front-45','ch 1 right','ch 3 right-45','ch 4 up','ch 6 front'})
+end
 
-figure
-plot(wl,Spec_ch0_Spec(2,:),'b'); hold on;
-plot(wl,Spec_ch1_Spec(2,:),'c');
-plot(wl,Spec_ch3_Spec(2,:),'g');
-plot(wl,Spec_ch4_Spec(2,:),'m');
-plot(wl,Spec_ch6_Spec(2,:),'r'); hold off;
-
+end
