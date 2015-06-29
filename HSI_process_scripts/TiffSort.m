a2c999b01ac4a35f45ffda389115df1a9b6bd560
Hannah Weller <hiweller@uchicago.edu>
2015-06-29 14:28:19 -0400

    successfully (?) finished TiffSort function

--- HSI_process_scripts/TiffSort.m
+++ HSI_process_scripts/TiffSort.m
@@ -1,15 +1,24 @@
 function TiffSort(Date)
+% sorts the individual tiff files into folders according to name
 % ex: TiffSort('Jun29')
 
-directoryRead = dir(Date);
+directoryRead = dir(['../',Date]);
 %fileNames = directoryRead.name;
-directoryRead = directoryRead(arrayfun(@(x)x.name(1),
-    directoryRead) ~='.');
+directoryRead = directoryRead(arrayfun(@(x)x.name(1), directoryRead) ~='.');
 
+FileID = directoryRead(1).name(1:18)
+mkdir(FileID)
 
-
-
-
+for i = 1:length(directoryRead)
+    if directoryRead(i).name(1:18) == FileID
+        movefile(directoryRead(i).name, FileID)
+        i = i+1
+    else
+        FileID = directoryRead(i).name(1:18)
+        mkdir(FileID)
+        i = i+1
+    end
 end
 
-fileName = dir('../../HSIData/Jun29')
+    
+    
