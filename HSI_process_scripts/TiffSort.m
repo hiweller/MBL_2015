6a72515c3a80644d953864534a1e64bd4aecb95f
arummel25 <arummel25@gmail.com>
2015-06-29 14:37:31 -0400

    ??

--- HSI_process_scripts/TiffSort.m
+++ HSI_process_scripts/TiffSort.m
@@ -4,15 +4,11 @@ function TiffSort(Date)
 
 directoryRead = dir(['../',Date]);
 %fileNames = directoryRead.name;
-<<<<<<< HEAD
+
 directoryRead = directoryRead(arrayfun(@(x)x.name(1),
     directoryRead) ~='.'); %remove hidden files
 
-
-
-=======
 directoryRead = directoryRead(arrayfun(@(x)x.name(1), directoryRead) ~='.');
->>>>>>> 75a2bebfd860b6a0eec1de02d14b7e857c534a15
 
 FileID = directoryRead(1).name(1:18)
 mkdir(FileID)
