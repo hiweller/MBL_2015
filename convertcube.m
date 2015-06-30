00db897b290f5d356f5ad64ba178f06675ca44e7
arummel25 <arummel25@gmail.com>
2015-06-30 16:29:38 -0400

    convertcube trying to figure out subplots

--- convertcube.m
+++ convertcube.m
@@ -12,14 +12,14 @@ imagedir = imagedir(arrayfun(@(x)x.name(1),imagedir) ~='.'); %remove hidden file
 for i = 1:length(imagedir); 
     fid = fopen(imagedir(i).name);
     cuberead = fread(fid,[2048 2048],'uint16');
-%     figure(i); subplot(4,4,1);
+    figure(i); 
     for j = 1:4
         for k = 1:4
         ch = (cuberead(j:4:2048, k:4:2048));
         channel = sprintf('chan',j,'.tiff');
         imagesc(ch); 
-%         figure; subplot(j,k,1);
         outname = sprintf('%s%s%s',imagedir(i).name,'.',channel);
+        subplot(4,4,k+4*(j-1));
         end
     end  
 end
