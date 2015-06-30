ba5bbf8f5265db98c2465f6fc05156faadd2332a
Hannah Weller <hiweller@uchicago.edu>
2015-06-30 14:39:01 -0400

    convertcube merging

--- convertcube.m
+++ convertcube.m
@@ -11,26 +11,29 @@ for i = 1:length(imagedir);
     fid = fopen(imagedir(i).name);
     cuberead = fread(fid,[2048 2048],'uint16');
     for j = 1:4
-        ch = (cuberead(j:4:2560, 1:4:2160));
-        channel = sprintf('%s%02d%s','chan',j,'.tiff');
-        imagesc(ch);
-        hold
-        outname = sprintf('%s%s%s',imagedir_input(i).name,'.',channel);
-   
+        for k = 1:4
+            ch = (cuberead(j:4:2048, k:4:2048);
+            channel = sprintf('%s%02d%s','chan', j, '.tiff');
+            imagesc(ch);
+            hold
+            outname=sprintf('%s%s%s',imagedir_input(i).name,'.',channel);
+        end
     end
+%         ch = (cuberead(j:4:2560, 1:4:2160));
+%         channel = sprintf('%s%02d%s','chan',j,'.tiff');
+%         imagesc(ch);
+%         hold
+%         outname = sprintf('%s%s%s',imagedir_input(i).name,'.',channel);
+%    
+%     end
     
-    for j = 5:8
-        ch = (cuberead(j:4:2048, 2:4:
-        
-    end
-    
-    for j = 9:12 
-    end
-    
-    for j = 13:16
-    end
-    
-       
-        
-    
-end
+%     for j = 5:8
+%         ch = (cuberead(j:4:2048, 2:4:
+%         
+%     end
+%     
+%     for j = 9:12 
+%     end
+%     
+%     for j = 13:16
+%     end
