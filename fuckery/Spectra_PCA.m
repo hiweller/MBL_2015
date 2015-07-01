7856aa1c19b3ec6e8ec1c0723f3d1da1304f4ba2
arummel25 <arummel25@gmail.com>
2015-07-01 15:55:09 -0400

    random comments in spec pca function

---
+++ fuckery/Spectra_PCA.m
@@ -0,0 +1,9 @@
+function Spectra_PCA(group1, group2) 
+
+% find all .dat files in directory 
+% each .dat file contains data from one channel from diff measurements
+% transpose the .dat matrix
+
+
+
+end
