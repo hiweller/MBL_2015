fa9dee7a8c338a7f033ec98a2a1d212d2bc2c39c
Hannah Weller <hiweller@uchicago.edu>
2015-07-01 15:55:09 -0400

    i have done nothing productive

---
+++ fuckery/Spectra_PCA.m
@@ -0,0 +1,5 @@
+function Spectra_PCA(Group1, Group2)
+% assumes you already ran ProcessSpec and have .dat files for comparison
+
+end
+% function(Group1, Group2) and compares them channel by channel using PCA
