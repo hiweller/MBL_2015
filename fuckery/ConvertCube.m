dfe76d104576b24aebc38dd7b2be880baa7f62dc
Hannah Weller <hiweller@uchicago.edu>
2015-07-15 14:06:22 -0400

    fixed spacing in subplots using subaxis instead

--- fuckery/ConvertCube.m
+++ fuckery/ConvertCube.m
@@ -16,22 +16,23 @@ for i = 1:length(imagedir);
     cuberead = fread(fopen(imagedir(i).name),[2048 2048],'uint16');
     FolderID = imagedir(i).name(1:end-3);
     mkdir(FolderID) % make directory to fill with tiffs
-    figure(i); 
+    figure(i);
+    
     for j = 1:4
         for k = 1:4 
-            subplot(4,4,k+4*(j-1)); % plot all 16 channels in one figure
+            subaxis(4,4,k+4*(j-1), 'Spacing', 0.01); % plot all 16 channels in one figure
             ch_unflip = (cuberead(j:4:2048, k:4:2048)); % 512x512 grid of 4x4 squares containing pixels for each channel
             ch = flip(ch_unflip, 2); % image inverted
             channel = sprintf('%s%d%s','chan',k+4*(j-1),'.tif');
             albert = max(max(ch));
-            imshow(ch, [0 albert]) % make image
+            imshow(ch, [0 albert], 'border', 'tight') % make image
 %             colormap(gray); % convert to grayscale - does this work?
             % what is this doing?
             outname = sprintf('%s%s%s',imagedir(i).name,'.',channel);
             imwrite(uint16(ch), [FolderID, '/', outname]);
         end
     end
-    tightfig
+   
 end
 
 end
