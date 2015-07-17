bd80e2a890349ea5ccdb450eec60fe69f6d72423
Hannah Weller <hiweller@uchicago.edu>
2015-07-17 15:22:31 -0400

    modified TiffSort so it names folders in a way that ShowHSIimage recognizes. currently giving me 'directory already exists' warnings despite the fact that the directories do not, in fact, exist beforehand, but otherwise seems to work just fine. not sure how to use the HSI process scripts though.

--- HSI_process_scripts/TiffSort.m
+++ HSI_process_scripts/TiffSort.m
@@ -1,3 +1,28 @@
+% function TiffSort(Date)
+% 
+% directoryRead = dir(['../',Date, '/*.tif*']);
+% 
+% FileID = directoryRead(1).name(1:(end-13));
+% mkdir(FileID)
+% 
+% for i = 1:length(directoryRead)
+%     if length(directoryRead(i).name(1:(end-13))) == length(FileID)
+%         if directoryRead(i).name(1:(end-13)) == FileID
+%             movefile(directoryRead(i).name, FileID);
+%         else
+%             FileID = directoryRead(i).name(1:(end-13))
+%             mkdir(FileID);
+% %         blah blah balh
+%         end
+%     else
+%         FileID = directoryRead(i).name(1:(end-13));
+%         mkdir(FileID);
+%     end
+% end
+% 
+% end
+
+
 function TiffSort(Date)
 % sorts the individual tiff files into folders according to name
 % ex: TiffSort('Jun29')
@@ -5,23 +30,27 @@ function TiffSort(Date)
 % ex: be in the Jun29 folder when you execute TiffSort('Jun29')
 
 
-directoryRead = dir(['../',Date]);
+directoryRead = dir(['../',Date, '/*.tiff']);
 %fileNames = directoryRead.name;
 
 directoryRead = directoryRead(arrayfun(@(x)x.name(1), directoryRead) ~='.'); %remove hidden files
 
-FileID = directoryRead(1).name(1:18)
-mkdir(FileID)
+FileID = directoryRead(1).name(1:18);
+FolderID = directoryRead(1).name(1:(end-15));
+mkdir(FolderID);
 
 for i = 1:length(directoryRead)
     if directoryRead(i).name(1:18) == FileID
-        movefile(directoryRead(i).name, FileID)
-        i = i+1
+        movefile(directoryRead(i).name, FolderID);
     else
-        FileID = directoryRead(i).name(1:18)
-        mkdir(FileID)
-        i = i+1
+        FileID = directoryRead(i).name(1:18);
+        FolderID = directoryRead(i).name(1:(end-15));
+        mkdir(directoryRead(i).name(1:(end-15)));
+        movefile(directoryRead(i).name, FolderID);
     end
+end
+
+
 end
 
     
