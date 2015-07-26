baf2f17f6be0cd4a53653ba64feb824a7c473acf
Hannah Weller <hiweller@uchicago.edu>
2015-07-26 16:52:53 -0400

    added ShowCube function for examining HSI data cubes

---
+++ fuckery/ShowCube.m
@@ -0,0 +1,19 @@
+function ShowCube(Date)
+% shows ch 1 image without saving/sorting any files
+% for use in checking image w/o processing
+% should be in the directory
+% ex: ShowCube('Jul31')
+
+imagedir = dir(['../', Date, '/*.3d']);
+
+for i = 1:length(imagedir)
+    cuberead = fread(fopen(imagedir(i).name), [2048 2048], 'uint16');
+    figure;
+    ch_unflip = cuberead(1:4:2048, 1:4:2048);
+    ch = flip(ch_unflip, 2);
+    mrmaximum = max(max(ch));
+    imshow(ch, [0 mrmaximum]);
+    title(imagedir(i).name);
+end
+
+end
