6bc3e569c81e616f085b6b5d58491ad0d8fc3cae
sepalmer <sepalmer@uchicago.edu>
2015-08-19 12:14:07 -0400

    Update ShowCube.m
    using imagesc, so figure window remains resized and located on auxiliary screen

--- Cubes/ShowCube.m
+++ Cubes/ShowCube.m
@@ -1,22 +1,24 @@
-function ShowCube(Flounder)
+function ShowCube(Lizards)
 % shows ch 1 image without saving/sorting any files
 % for use in checking image w/o processing
-% should be in the directory
 % ex: ShowCube('Jul31')
 
-imagedir = dir([Flounder, '/*.3d']);
+imagedir = dir([Lizards, '/*.3d']);
+
+fig = figure;
 
 for i = 1:length(imagedir)
-    cuberead = fread(fopen(imagedir(i).name), [2048 2048], 'uint16');
-    figure;
+    cuberead = fread(fopen([Lizards '/' imagedir(i).name]), [2048 2048], 'uint16');
     ch_unflip = cuberead(1:4:2048, 1:4:2048);
     ch = flip(ch_unflip, 2);
     mrmaximum = max(max(ch));
-    imshow(ch, [0 mrmaximum]);
-    title(imagedir(i).name);
+    figure(fig);
+    imagesc(ch, [0 mrmaximum]);
+    colormap(gray);
+    axis square;
+    axis off;
+    title(imagedir(i).name,'Interpreter','none');
+    pause
 end
 
 end
-
-
-
